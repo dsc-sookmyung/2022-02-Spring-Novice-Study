f8ee1c25ec71ba277517915675294b3a0b65b97d
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-10-02 13:53:33 +0900

    [feat] HelloController와 Test 코드 작성

---
+++ 황연진/gdsc-springboot-study/src/test/java/com/gdsc/study/gdscspringbootstudy/web/HelloControllerTest.java
@@ -0,0 +1,29 @@
+package com.gdsc.study.gdscspringbootstudy.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello_return() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
