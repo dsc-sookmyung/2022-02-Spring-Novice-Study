0075ffe31bb091dcf7b5b0792c3d7ecd5f015543
dayoung <adask13@naver.com>
2022-10-02 09:27:57 +0900

    lombok

--- 최다영/springProject-1/src/main/java/com/example/springProject1/web/HelloController.java
+++ 최다영/springProject-1/src/main/java/com/example/springProject1/web/HelloController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+//
+//
 @RestController
 public class HelloController {
     @GetMapping("/hello")
