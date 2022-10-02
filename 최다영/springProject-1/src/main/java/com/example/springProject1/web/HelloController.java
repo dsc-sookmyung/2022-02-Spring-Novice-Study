720743d55ab7e78450e85ed493457290f3ce6474
dayoung <adask13@naver.com>
2022-10-02 09:33:10 +0900

    lombok

--- 최다영/springProject-1/src/main/java/com/example/springProject1/web/HelloController.java
+++ 최다영/springProject-1/src/main/java/com/example/springProject1/web/HelloController.java
@@ -5,9 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
-//
-//
 @RestController
 public class HelloController {
     @GetMapping("/hello")
--- 최다영/springProject-1/src/test/java/com/example/springProject1/dto/HelloResponseDtoTest.java
+++ 최다영/springProject-1/src/test/java/com/example/springProject1/dto/HelloResponseDtoTest.java
@@ -5,6 +5,7 @@ import com.example.springProject1.web.dto.HelloResponseDto;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
+
 public class HelloResponseDtoTest {
     @Test
     public void 롬복_기능_테스트(){
