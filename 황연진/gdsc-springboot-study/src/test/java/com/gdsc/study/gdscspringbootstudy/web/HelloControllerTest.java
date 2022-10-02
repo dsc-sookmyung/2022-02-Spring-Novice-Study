d4c4900d1609a96d8f3f1b2ed71a96c14a5ba616
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-10-02 14:09:42 +0900

    [feat] lombok 적용한 코드 추가

--- 황연진/gdsc-springboot-study/src/test/java/com/gdsc/study/gdscspringbootstudy/web/HelloControllerTest.java
+++ 황연진/gdsc-springboot-study/src/test/java/com/gdsc/study/gdscspringbootstudy/web/HelloControllerTest.java
@@ -7,9 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -25,5 +25,18 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto_return() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
