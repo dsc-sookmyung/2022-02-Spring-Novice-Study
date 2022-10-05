26ce97b8ce5451b9b42ef46f859f06425878059a
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-10-05 15:43:09 +0900

    [feat] 수정/조회 API 만들기

--- 황연진/gdsc-springboot-study/src/main/java/com/gdsc/study/gdscspringbootstudy/service/posts/PostsService.java
+++ 황연진/gdsc-springboot-study/src/main/java/com/gdsc/study/gdscspringbootstudy/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.gdsc.study.gdscspringbootstudy.service.posts;
 
+import com.gdsc.study.gdscspringbootstudy.domain.posts.Posts;
 import com.gdsc.study.gdscspringbootstudy.domain.posts.PostsRepository;
+import com.gdsc.study.gdscspringbootstudy.web.dto.PostsResponseDto;
 import com.gdsc.study.gdscspringbootstudy.web.dto.PostsSaveRequestDto;
+import com.gdsc.study.gdscspringbootstudy.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,17 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
     }
 }
--- 황연진/gdsc-springboot-study/src/main/java/com/gdsc/study/gdscspringbootstudy/web/PostsApiController.java
+++ 황연진/gdsc-springboot-study/src/main/java/com/gdsc/study/gdscspringbootstudy/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.gdsc.study.gdscspringbootstudy.web;
 
 import com.gdsc.study.gdscspringbootstudy.service.posts.PostsService;
+import com.gdsc.study.gdscspringbootstudy.web.dto.PostsResponseDto;
 import com.gdsc.study.gdscspringbootstudy.web.dto.PostsSaveRequestDto;
+import com.gdsc.study.gdscspringbootstudy.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,5 +16,15 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
