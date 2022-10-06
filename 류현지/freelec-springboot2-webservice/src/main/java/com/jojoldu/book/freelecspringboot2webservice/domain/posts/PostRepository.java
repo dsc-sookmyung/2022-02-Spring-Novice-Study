cb7165df1a354767b754ba97c412e1f32d542627
Ryuuhj <hre1999@sookmyung.ac.kr>
2022-10-07 02:03:34 +0900

    chore : JpaRepository 생성 - DB Layer 접근자

---
+++ 류현지/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/freelecspringboot2webservice/domain/posts/PostRepository.java
@@ -0,0 +1,7 @@
+package com.jojoldu.book.freelecspringboot2webservice.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Posts, Long> {
+
+}
