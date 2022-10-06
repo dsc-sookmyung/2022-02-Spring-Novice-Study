d186102ea8ee2d82ffdb2ce3e19fa217edad3bc6
Ryuuhj <hre1999@sookmyung.ac.kr>
2022-10-07 00:41:44 +0900

    feat : posts table 설계

---
+++ 류현지/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/freelecspringboot2webservice/domain/posts/Posts.java
@@ -0,0 +1,34 @@
+package com.jojoldu.book.freelecspringboot2webservice.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+//*어노테이션 순서 - 주요 어노테이션을 클래스와 가깝게
+
+@Getter //lombok
+@NoArgsConstructor //lombok
+@Entity //<< 주요 어노테이션
+public class Posts {  //실제 DB table과 매칭 될 클래스 == Entity 클래스
+    //id 정의
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
