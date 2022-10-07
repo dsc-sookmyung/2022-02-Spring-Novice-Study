9bf96e416bebaeb3afc627ad94ff565945db4e2c
bnfkim <kgr26@MacBook-Pro.local>
2022-10-08 00:31:57 +0900

    [feat]등록/수정/조회 API 만들기

--- 김규리/freelec-springboot2-webservice/src/test/java/com/jojoldu/book/web/PostApiControllerTest.java
+++ 김규리/freelec-springboot2-webservice/src/test/java/com/jojoldu/book/web/PostApiControllerTest.java
@@ -1,4 +1,52 @@
-import junit.framework.TestCase;
-public class PostApiControllerTest extends TestCase {
-  
+package com.jojoldu.book.web;
+
+import com.jojoldu.book.domain.posts.PostRepository;
+import com.jojoldu.book.web.dto.PostsSaveRequestDto;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PostApiControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @After
+    public void tearDown() throws Exception {
+        postRepository.deleteAll();
+    }
+
+    @Test
+    public void Posts_등록된다() throws Exception {
+        //given
+        String title = "title";
+        String content = "content";
+        PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
+                .title(title)
+                .content(content)
+                .author("author")
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts";
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+
+        //then
+    }
+
 }
