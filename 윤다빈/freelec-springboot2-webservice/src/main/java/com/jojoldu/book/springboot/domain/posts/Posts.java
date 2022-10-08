db7332098d625c491a11b5396b16186587561f5f
yoondabin <yundabin0608@gmail.com>
2022-10-08 15:37:57 +0900

    [2주차] Spring Data Jpa 적용 - Posts

---
+++ 윤다빈/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,36 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor     // @Getter 와 @NoArgsConstructor(기본 생성자 자동 추가, public Post(){}의 효과) 는 롬복의 어노테이션으로 필수 아님
+@Entity                // JPA의 어노테이션으로 필수라 클래스에 가까이 뒀음
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder  // 해당 클래스의 빌더 패턴 클래스 생성
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
