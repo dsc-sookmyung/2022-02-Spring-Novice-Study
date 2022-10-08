5f6a7deae8610f324145990a7bfe54fbba74a63c
ChaeyeonHan <kate2236e@naver.com>
2022-10-08 15:48:34 +0900

    feat: jpa auditing 추가

--- 한채연/freelec-springboot2-webservice/src/main/java/com/hcy/book/springboot/domain/BaseTimeEntity.java
+++ 한채연/freelec-springboot2-webservice/src/main/java/com/hcy/book/springboot/domain/BaseTimeEntity.java
@@ -1,2 +1,22 @@
-package com.hcy.book.springboot.domain;public class BaseTimeEntity {
+package com.hcy.book.springboot.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;  // 엔티티가 생성된 시간
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;  // 엔티티 값을 변경한 시간
 }
