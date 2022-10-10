cb3f692cfa379cd4e0cfd13345c6646d2d55bd46
ChaeyeonHan <kate2236e@naver.com>
2022-10-10 12:47:13 +0900

    feat: 게시글 등록 화면

--- 한채연/freelec-springboot2-webservice/src/main/java/com/hcy/book/springboot/web/IndexController.java
+++ 한채연/freelec-springboot2-webservice/src/main/java/com/hcy/book/springboot/web/IndexController.java
@@ -10,4 +10,10 @@ public class IndexController {
     public String index(){
         return "index";
     }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+
 }
