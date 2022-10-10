6f83da293be725df2f4ef524c724c90891ee8c6e
ChaeyeonHan <kate2236e@naver.com>
2022-10-10 13:11:34 +0900

    feat: 게시글 조회 화면

--- 한채연/freelec-springboot2-webservice/src/main/java/com/hcy/book/springboot/web/IndexController.java
+++ 한채연/freelec-springboot2-webservice/src/main/java/com/hcy/book/springboot/web/IndexController.java
@@ -1,19 +1,31 @@
 package com.hcy.book.springboot.web;
 
+import com.hcy.book.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index(){
-        return "index";
-    }
+    private final PostsService postsService;
+
+//    @GetMapping("/")
+//    public String index(){
+//        return "index";
+//    }
 
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc()); // 메서드 결과로 가져온 posts를 index.mustache에 전달한다
+        return "index";
     }
 
 }
