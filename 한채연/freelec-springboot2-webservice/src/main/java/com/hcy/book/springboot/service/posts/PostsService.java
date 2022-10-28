8df95e72c4bb008192e93902bcc8b4f9cda85258
ChaeyeonHan <kate2236e@naver.com>
2022-10-28 23:28:20 +0900

    feat: 게시글 삭제 화면

--- 한채연/freelec-springboot2-webservice/src/main/java/com/hcy/book/springboot/service/posts/PostsService.java
+++ 한채연/freelec-springboot2-webservice/src/main/java/com/hcy/book/springboot/service/posts/PostsService.java
@@ -35,6 +35,14 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id= " + id));
