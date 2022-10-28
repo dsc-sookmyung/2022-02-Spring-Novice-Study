e68150e815ecd5ff9ac04ca88e8dd43a92c07ad3
ChaeyeonHan <kate2236e@naver.com>
2022-10-28 23:10:42 +0900

    feat: 게시글 수정 화면

--- 한채연/freelec-springboot2-webservice/src/main/java/com/hcy/book/springboot/web/IndexController.java
+++ 한채연/freelec-springboot2-webservice/src/main/java/com/hcy/book/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.hcy.book.springboot.web;
 
 import com.hcy.book.springboot.service.posts.PostsService;
+import com.hcy.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -14,7 +16,7 @@ public class IndexController {
 
 //    @GetMapping("/")
 //    public String index(){
-//        return "index";
+//        return "index";  // src/main/resources/templates/index.mustache
 //    }
 
     @GetMapping("/posts/save")
@@ -26,6 +28,14 @@ public class IndexController {
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc()); // 메서드 결과로 가져온 posts를 index.mustache에 전달한다
         return "index";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 
 }
