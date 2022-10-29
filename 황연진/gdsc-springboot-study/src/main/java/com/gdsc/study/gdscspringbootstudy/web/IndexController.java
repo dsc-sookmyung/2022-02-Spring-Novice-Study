08c20b38434df92ac7933db3e8efdf84578cb66d
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-10-30 01:50:59 +0900

    [feat] mustache로 화면 구성하고 글 등록/목록 기능 추가

---
+++ 황연진/gdsc-springboot-study/src/main/java/com/gdsc/study/gdscspringbootstudy/web/IndexController.java
@@ -0,0 +1,25 @@
+package com.gdsc.study.gdscspringbootstudy.web;
+
+import com.gdsc.study.gdscspringbootstudy.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+}
