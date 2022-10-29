4ff624f87d6d6b1b868717d818c93898896d5162
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-10-30 02:29:35 +0900

    [refactor] 중복 url 매핑

--- 황연진/gdsc-springboot-study/src/main/java/com/gdsc/study/gdscspringbootstudy/web/PostsApiController.java
+++ 황연진/gdsc-springboot-study/src/main/java/com/gdsc/study/gdscspringbootstudy/web/PostsApiController.java
@@ -1,36 +1,45 @@
 package com.gdsc.study.gdscspringbootstudy.web;
 
 import com.gdsc.study.gdscspringbootstudy.service.posts.PostsService;
+import com.gdsc.study.gdscspringbootstudy.web.dto.PostsListResponseDto;
 import com.gdsc.study.gdscspringbootstudy.web.dto.PostsResponseDto;
 import com.gdsc.study.gdscspringbootstudy.web.dto.PostsSaveRequestDto;
 import com.gdsc.study.gdscspringbootstudy.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
+@RequestMapping("api/v1/posts")
 @RestController
 public class PostsApiController {
 
     private final PostsService postsService;
 
-    @PostMapping("/api/v1/posts")
+    @PostMapping
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
 
-    @PutMapping("/api/v1/posts/{id}")
+    @PutMapping("/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
     }
 
-    @GetMapping("/api/v1/posts/{id}")
+    @GetMapping("/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
     }
 
-    @DeleteMapping("/api/v1/posts/{id}")
+    @DeleteMapping("/{id}")
     public Long delete(@PathVariable Long id) {
         postsService.delete(id);
         return id;
+    }
+
+    @GetMapping
+    public List<PostsListResponseDto> getPostsList(){
+        return postsService.findAllDesc();
     }
 }
