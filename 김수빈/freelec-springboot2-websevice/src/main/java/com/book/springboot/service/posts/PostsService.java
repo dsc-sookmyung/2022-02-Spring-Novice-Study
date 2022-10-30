8bfc37411184cbbd964a687a1dddfb236ef3fe09
Subin <bina@sookmyung.ac.kr>
2022-10-31 03:03:22 +0900

    Feat: 3주차 내용 커밋

--- 김수빈/freelec-springboot2-websevice/src/main/java/com/book/springboot/service/posts/PostsService.java
+++ 김수빈/freelec-springboot2-websevice/src/main/java/com/book/springboot/service/posts/PostsService.java
@@ -11,7 +11,10 @@ import com.book.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.book.springboot.web.dto.PostsListResponseDto;
 
+import java.util.List;
+import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Service
 public class PostsService {
@@ -34,5 +37,19 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+        postsRepository.delete(posts);
     }
 }
