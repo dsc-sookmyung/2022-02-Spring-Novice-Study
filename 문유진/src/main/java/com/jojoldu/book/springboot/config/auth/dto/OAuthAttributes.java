d473b2f7ecbedad21800858a43efebd99f2b7b68
Moonyujin <yujin0419@sookmyung.ac.kr>
2022-10-31 18:15:06 +0900

    Feat: 5.6 네이버 로그인

--- 문유진/src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
+++ 문유진/src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -25,7 +25,21 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    public static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     public static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
