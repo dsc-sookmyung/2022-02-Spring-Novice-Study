16099cd773bf46fc61cd7723db22ea90eb63e6b6
happine2s <happine2s@sookmyung.ac.kr>
2022-10-31 23:56:22 +0900

    [2주차] JPA Auditing으로 생성/수정 시간 자동화

--- 박소윤/week02/src/main/java/com/gdsc/week02/Week02Application.java
+++ 박소윤/week02/src/main/java/com/gdsc/week02/Week02Application.java
@@ -2,8 +2,10 @@ package com.gdsc.week02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Week02Application {
 
     public static void main(String[] args) {
--- 박소윤/week02/src/test/java/com/gdsc/week02/web/PostsApiControllerTest.java
+++ 박소윤/week02/src/test/java/com/gdsc/week02/web/PostsApiControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -94,5 +95,26 @@ public class PostsApiControllerTest {
         List<Posts> all =postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(exceptedTitle);
         assertThat(all.get(0).getContent()).isEqualTo(exceptedContent);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록(){
+        //given
+        LocalDateTime now=LocalDateTime.of(2022,10,7,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts=postsList.get(0);
+
+        System.out.println(">>>>>>>> created Date="+posts.getCreatedDate()+", modified Date="+posts.getModifiedDate());
+        assertThat(posts.getCreatedDate().isAfter(now));
+        assertThat(posts.getModifiedDate().isAfter(now));
     }
 }
