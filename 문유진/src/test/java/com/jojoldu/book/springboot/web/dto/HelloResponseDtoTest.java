8a5141a3399ac8d29ae196fa8286e66df886bb98
Moonyujin <yujin0419@sookmyung.ac.kr>
2022-11-01 03:09:53 +0900

    Chore: JUnit4 to 5 및 gradle 버전 변경

--- 문유진/src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
+++ 문유진/src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -1,6 +1,7 @@
 package com.jojoldu.book.springboot.web.dto;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HelloResponseDtoTest {
