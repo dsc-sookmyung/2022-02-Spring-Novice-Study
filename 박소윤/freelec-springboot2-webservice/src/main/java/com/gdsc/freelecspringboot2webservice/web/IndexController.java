2dff821a87405bfa693fcbddab8ac04edbb1df3e
happine2s <happine2s@sookmyung.ac.kr>
2022-11-01 12:40:47 +0900

    [3주차] 게시글 등록 화면 만들기

--- 박소윤/freelec-springboot2-webservice/src/main/java/com/gdsc/freelecspringboot2webservice/web/IndexController.java
+++ 박소윤/freelec-springboot2-webservice/src/main/java/com/gdsc/freelecspringboot2webservice/web/IndexController.java
@@ -9,4 +9,9 @@ public class IndexController {
     public String index(){
         return "index";
     }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
 }
