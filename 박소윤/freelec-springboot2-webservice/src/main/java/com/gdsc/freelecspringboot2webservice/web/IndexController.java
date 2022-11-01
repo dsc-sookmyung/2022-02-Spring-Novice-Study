875ef1a457e15d33625849cc32205c06d10f62e1
happine2s <happine2s@sookmyung.ac.kr>
2022-11-01 13:43:18 +0900

    [3주차] 게시글 수정, 삭제 화면 만들기

--- 박소윤/freelec-springboot2-webservice/src/main/java/com/gdsc/freelecspringboot2webservice/web/IndexController.java
+++ 박소윤/freelec-springboot2-webservice/src/main/java/com/gdsc/freelecspringboot2webservice/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.gdsc.freelecspringboot2webservice.web;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.ui.Model;
 import com.gdsc.freelecspringboot2webservice.service.posts.PostsService;
+import com.gdsc.freelecspringboot2webservice.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,5 +22,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto=postsService.findById(id);
+        model.addAttribute("posts",dto);
+
+        return "posts-update";
     }
 }
