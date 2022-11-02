d35f0f9f50c1a78099b63130d2c7905605cbbd86
happine2s <happine2s@sookmyung.ac.kr>
2022-11-02 23:39:49 +0900

    [3주차] 추가실습

--- 박소윤/freelec-springboot2-webservice/src/main/java/com/gdsc/freelecspringboot2webservice/web/PostsApiController.java
+++ 박소윤/freelec-springboot2-webservice/src/main/java/com/gdsc/freelecspringboot2webservice/web/PostsApiController.java
@@ -1,5 +1,7 @@
 package com.gdsc.freelecspringboot2webservice.web;
 
+import com.gdsc.freelecspringboot2webservice.domain.posts.Posts;
+import com.gdsc.freelecspringboot2webservice.domain.posts.PostsRepository;
 import com.gdsc.freelecspringboot2webservice.service.posts.PostsService;
 import com.gdsc.freelecspringboot2webservice.web.dto.PostsResponseDto;
 import com.gdsc.freelecspringboot2webservice.web.dto.PostsSaveRequestDto;
@@ -7,27 +9,36 @@ import com.gdsc.freelecspringboot2webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/v1/posts")
 public class PostsApiController {
     private final PostsService postsService;
+    private final PostsRepository postsRepository;
 
-    @PostMapping("/api/v1/posts")
+    @PostMapping
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
     }
 
-    @PutMapping("/api/v1/posts/{id}")
+    @PutMapping("/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
         return postsService.update(id, requestDto);
     }
 
-    @GetMapping("/api/v1/posts/{id}")
+    @GetMapping("/{id}")
     public PostsResponseDto findById(@PathVariable Long id){
         return postsService.findById(id);
     }
 
-    @DeleteMapping("/api/v1/posts/{id}")
+    @GetMapping
+    public List<Posts> getCourses(){
+        return postsRepository.findAllDesc();
+    }
+
+    @DeleteMapping("/{id}")
     public Long delete(@PathVariable Long id){
         postsService.delete(id);
         return id;
