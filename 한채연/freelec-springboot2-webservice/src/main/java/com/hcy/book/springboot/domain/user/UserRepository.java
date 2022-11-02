8c08ff55972737e49cf124e402b810fd9ad358a4
ChaeyeonHan <kate2236e@naver.com>
2022-11-02 13:02:26 +0900

    feat: user 엔티티 생성

---
+++ 한채연/freelec-springboot2-webservice/src/main/java/com/hcy/book/springboot/domain/user/UserRepository.java
@@ -0,0 +1,10 @@
+package com.hcy.book.springboot.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<Long, User> {
+
+    Optional<User> findByEmail(String email);
+}
