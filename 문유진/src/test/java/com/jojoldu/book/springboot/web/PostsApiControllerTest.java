dccadbe66b033424b130a6e20bb6bd16298302c6
Moonyujin <yujin0419@sookmyung.ac.kr>
2022-11-04 02:37:38 +0900

    Chore: PR 보내기 위한 작업

--- 문유진/src/test/java/com/jojoldu/book/springboot/web/PostsApiControllerTest.java
+++ 문유진/src/test/java/com/jojoldu/book/springboot/web/PostsApiControllerTest.java
@@ -60,7 +60,7 @@ public class PostsApiControllerTest {
 
     @Test
     @WithMockUser(roles = "USER")
-    public void Posts_등록() throws Exception {
+    public void Posts_등록된다() throws Exception {
         // given
         String title = "title";
         String content = "content";
