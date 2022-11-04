e64aa974113f7a1bfafd75ddc3d43f893a0ac6a3
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-11-05 01:37:17 +0900

    [feat] 네이버 로그인

--- 황연진/gdsc-springboot-study/src/main/java/com/gdsc/study/gdscspringbootstudy/config/auth/dto/OAuthAttributes.java
+++ 황연진/gdsc-springboot-study/src/main/java/com/gdsc/study/gdscspringbootstudy/config/auth/dto/OAuthAttributes.java
@@ -25,6 +25,9 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -34,6 +37,17 @@ public class OAuthAttributes {
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    public static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("picture"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
