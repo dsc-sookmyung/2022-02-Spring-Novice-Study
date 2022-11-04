d014eed27a3caf96c63e36839bdf48e3d11a1d70
ChaeyeonHan <kate2236e@naver.com>
2022-11-05 01:38:25 +0900

    feat: google login

--- 한채연/freelec-springboot2-webservice/src/main/java/com/hcy/book/springboot/domain/user/UserRepository.java
+++ 한채연/freelec-springboot2-webservice/src/main/java/com/hcy/book/springboot/domain/user/UserRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<Long, User> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 }
