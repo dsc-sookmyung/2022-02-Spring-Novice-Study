6edcbcc1d1e1a53927e63dceea727bdaa22e882b
yeoncheong <yjin1100@sookmyung.ac.kr>
2022-11-05 01:21:17 +0900

    [refactor] 어노테이션 기반으로 개선하기

--- 황연진/gdsc-springboot-study/src/main/java/com/gdsc/study/gdscspringbootstudy/web/IndexController.java
+++ 황연진/gdsc-springboot-study/src/main/java/com/gdsc/study/gdscspringbootstudy/web/IndexController.java
@@ -1,9 +1,11 @@
 package com.gdsc.study.gdscspringbootstudy.web;
 
+import com.gdsc.study.gdscspringbootstudy.config.auth.LoginUser;
 import com.gdsc.study.gdscspringbootstudy.config.auth.dto.SessionUser;
 import com.gdsc.study.gdscspringbootstudy.service.posts.PostsService;
 import com.gdsc.study.gdscspringbootstudy.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,12 +18,10 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null) {
             model.addAttribute("userName", user.getName());
         }
