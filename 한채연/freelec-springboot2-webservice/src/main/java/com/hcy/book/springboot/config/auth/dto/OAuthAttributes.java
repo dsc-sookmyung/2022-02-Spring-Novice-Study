f3b59ad7cfb3254be0da587bb54646717375c667
ChaeyeonHan <kate2236e@naver.com>
2022-11-05 15:12:41 +0900

    feat: naver login 추가

--- 한채연/freelec-springboot2-webservice/src/main/java/com/hcy/book/springboot/config/auth/dto/OAuthAttributes.java
+++ 한채연/freelec-springboot2-webservice/src/main/java/com/hcy/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -27,6 +27,10 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
+
+        if ("naver".equals(registrationId)){
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -36,6 +40,18 @@ public class OAuthAttributes {
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    public static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes){
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("picture"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
