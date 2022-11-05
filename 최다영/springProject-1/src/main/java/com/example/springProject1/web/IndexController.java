d9ebd3d24dbaa3273a87abdeb54b086df48d9608
dayoung <adask13@naver.com>
2022-11-06 03:55:47 +0900

    구글 서비스 등록

--- 최다영/springProject-1/src/main/java/com/example/springProject1/web/IndexController.java
+++ 최다영/springProject-1/src/main/java/com/example/springProject1/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.springProject1.web;
 
+import com.example.springProject1.config.auth.dto.SessionUser;
 import com.example.springProject1.service.posts.PostsService;
 import com.example.springProject1.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -7,15 +8,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
+
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts",postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
