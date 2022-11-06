ed41dd0851b0693f7466014f1dd686cd87e44332
AhyeonLim <ahyeon31@sookmyung.ac.kr>
2022-11-06 20:41:46 +0900

    4주차 스프링 시큐리티와 OAUTH2.0

--- 임아현/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/dto/IndexController.java
+++ 임아현/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/dto/IndexController.java
@@ -1,35 +1,42 @@
 package com.jojoldu.book.springboot.web.dto;
 
 
+import com.jojoldu.book.springboot.config.auth.LoginUser;
+import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.service.posts.PostsService;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
-//    @GetMapping("/")
-//    public String index(){
-//        return "index";  // src/main/resources/templates/index.mustache
-//    }
+    @GetMapping("/")
+    public String index(Model model) {
+        public String index (Model model, @LoginUser SessionUser user) {
+            model.addAttribute("posts", postsService.findAllDesc());
+            SessionUser user = (SessionUser) httpSession.getAttribute("user");
+            if(user != null) {
+                model.addAttribute("userName", user.getName());
+            }
+            return "index";
+        }
+    }
+
 
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
     }
 
-    @GetMapping("/")
-    public String index(Model model){
-        model.addAttribute("posts", postsService.findAllDesc()); // 메서드 결과로 가져온 posts를 index.mustache에 전달한다
-        return "index";
-    }
 
     @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model){
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
-
         return "posts-update";
     }
 
