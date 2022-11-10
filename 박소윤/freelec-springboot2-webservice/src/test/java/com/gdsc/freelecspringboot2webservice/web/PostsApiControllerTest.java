103a30b437b2230f7038f0777fcd5ef9fd125338
happine2s <happine2s@sookmyung.ac.kr>
2022-11-10 11:12:01 +0900

    [4주차] 기존 테스트에 시큐리티 적용하기

--- 박소윤/freelec-springboot2-webservice/src/test/java/com/gdsc/freelecspringboot2webservice/web/PostsApiControllerTest.java
+++ 박소윤/freelec-springboot2-webservice/src/test/java/com/gdsc/freelecspringboot2webservice/web/PostsApiControllerTest.java
@@ -1,10 +1,12 @@
 package com.gdsc.freelecspringboot2webservice.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gdsc.freelecspringboot2webservice.domain.posts.Posts;
 import com.gdsc.freelecspringboot2webservice.domain.posts.PostsRepository;
 import com.gdsc.freelecspringboot2webservice.web.dto.PostsSaveRequestDto;
 import com.gdsc.freelecspringboot2webservice.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,19 +14,35 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PostsApiControllerTest {
+    @Autowired
+    private WebApplicationContext context;
+    private MockMvc mvc;
+
+    @Before
+    public void setup(){
+        mvc= MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
     @LocalServerPort
     private int port;
 
@@ -40,6 +58,7 @@ public class PostsApiControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void Posts_등록된다() throws Exception{
         //given
         String title="title";
@@ -53,18 +72,19 @@ public class PostsApiControllerTest {
         String url="http://localhost:"+port+"/api/v1/posts";
 
         //when
-        ResponseEntity<Long> responseEntity=restTemplate.postForEntity(url,requestDto, Long.class);
+        mvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
 
         //then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
-
         List<Posts> all=postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void Posts_수정된다() throws Exception{
         //given
         Posts savedPosts=postsRepository.save(Posts.builder()
@@ -86,35 +106,16 @@ public class PostsApiControllerTest {
         HttpEntity<PostsUpdateRequestDto> requestEntity=new HttpEntity<>(requestDto);
 
         //when
-        ResponseEntity<Long> responseEntity=restTemplate.exchange(url, HttpMethod.PUT,requestEntity,Long.class);
+        mvc.perform(put(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
+
 
         //then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
-
         List<Posts> all =postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(exceptedTitle);
         assertThat(all.get(0).getContent()).isEqualTo(exceptedContent);
     }
 
-    @Test
-    public void BaseTimeEntity_등록(){
-        //given
-        LocalDateTime now=LocalDateTime.of(2022,10,7,0,0,0);
-        postsRepository.save(Posts.builder()
-                .title("title")
-                .content("content")
-                .author("author")
-                .build());
-
-        //when
-        List<Posts> postsList = postsRepository.findAll();
-
-        //then
-        Posts posts=postsList.get(0);
-
-        System.out.println(">>>>>>>> created Date="+posts.getCreatedDate()+", modified Date="+posts.getModifiedDate());
-        assertThat(posts.getCreatedDate().isAfter(now));
-        assertThat(posts.getModifiedDate().isAfter(now));
-    }
 }
