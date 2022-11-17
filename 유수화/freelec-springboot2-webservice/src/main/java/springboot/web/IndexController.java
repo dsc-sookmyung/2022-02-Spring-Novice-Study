66978c7f5819624089b0768e0048c9c6fed76cd6
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2022-11-17 18:05:16 +0900

    [4주차] 구글로그인 구현하다가 안됨

--- 유수화/freelec-springboot2-webservice/src/main/java/springboot/web/IndexController.java
+++ 유수화/freelec-springboot2-webservice/src/main/java/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package springboot.web;
 
+import config.auth.dto.SessionUser;
 import springboot.service.posts.PostsService;
 import springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +10,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null){
+            model.addAttribute("userName",user.getName());
+        }
+
         return "index";
     }
 
