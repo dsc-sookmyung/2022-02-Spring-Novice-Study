79bc2ab49dd3c89b4d591c1973ecaf6a06ac4771
Juuuic <sn990908a@gmail.com>
2022-11-19 14:39:44 +0900

    6주차 naver response수정

--- 정영주/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
+++ 정영주/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -46,9 +46,9 @@ public class OAuthAttributes { //dto로 간주
     private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
         Map<String, Object> response = (Map<String, Object>) attributes.get("response");
         return OAuthAttributes.builder()
-                .name((String) attributes.get("name"))
-                .email((String) attributes.get("email"))
-                .picture((String) attributes.get("profile_image"))
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
                 .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
