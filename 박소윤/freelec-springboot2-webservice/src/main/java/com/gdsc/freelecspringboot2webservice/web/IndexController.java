54e09fd4b91883038c6bf4f369a75c6b5fff373e
SoYun Park <102134838+happine2s@users.noreply.github.com>
2022-11-20 06:10:44 +0900

    Revert "[박소윤] 2022 GDSC Spring Study 입문 - 4주차"

--- 박소윤/freelec-springboot2-webservice/src/main/java/com/gdsc/freelecspringboot2webservice/web/IndexController.java
+++ 박소윤/freelec-springboot2-webservice/src/main/java/com/gdsc/freelecspringboot2webservice/web/IndexController.java
@@ -1,6 +1,5 @@
 package com.gdsc.freelecspringboot2webservice.web;
 
-import com.gdsc.freelecspringboot2webservice.config.auth.dto.SessionUser;
 import com.gdsc.freelecspringboot2webservice.service.posts.PostsService;
 import com.gdsc.freelecspringboot2webservice.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,23 +8,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
-
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-
-        if (user != null) {
-            model.addAttribute("loginUserName", user.getName());
-        }
+    public String index(Model model){
+        model.addAttribute("posts",postsService.findAllDesc());
         return "index";
     }
 
