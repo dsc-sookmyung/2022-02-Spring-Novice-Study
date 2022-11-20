870558bcf23ec1ad43479f9db487dcfe753a1999
Subin <bina@sookmyung.ac.kr>
2022-11-21 05:23:06 +0900

    수정

--- 김수빈/freelec-springboot2-websevice/src/main/java/com/book/springboot/web/PostsApiController.java
+++ 김수빈/freelec-springboot2-websevice/src/main/java/com/book/springboot/web/PostsApiController.java
@@ -1,46 +1,45 @@
 package com.book.springboot.web;
 
-import com.book.springboot.web.dto.PostsSaveRequestDto;
 import com.book.springboot.service.posts.PostsService;
+import com.book.springboot.web.dto.PostsListResponseDto;
 import com.book.springboot.web.dto.PostsResponseDto;
-import com.book.springboot.web.dto.PostsUpdateRequestDto; //
-
-import com.book.springboot.web.dto.PostsListResponseDto; //
-import java.util.List; //
-
+import com.book.springboot.web.dto.PostsSaveRequestDto;
+import com.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/posts")
 @RestController
 public class PostsApiController {
 
     private final PostsService postsService;
 
-    @PostMapping("/api/v1/posts")
-    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+    @PostMapping
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
     }
 
-    @PostMapping("/api/v1/posts/{id}")
-    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+    @PutMapping("{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
         return postsService.update(id, requestDto);
     }
 
-    @GetMapping("/api/v1/posts/{id}")
-    public PostsResponseDto findById (@PathVariable Long id) {
+    @GetMapping("{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
         return postsService.findById(id);
     }
 
-    @DeleteMapping("/api/v1/posts/{id}")
-    public Long delete(@PathVariable Long id) {
+    @DeleteMapping("{id}")
+    public Long delete(@PathVariable Long id){
         postsService.delete(id);
         return id;
     }
-    //목록 얻어오기
-    @GetMapping("/api/v1/posts/")
+
+    @GetMapping
     public List<PostsListResponseDto> getPostsList(){
         return postsService.findAllDesc();
     }
-
 }
