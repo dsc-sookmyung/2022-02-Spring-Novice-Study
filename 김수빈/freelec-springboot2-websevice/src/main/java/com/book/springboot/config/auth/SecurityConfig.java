fc80d769baca8ddde8d45a3a524caf3cde356665
Subin <bina@sookmyung.ac.kr>
2022-11-27 04:51:29 +0900

    수정

--- 김수빈/freelec-springboot2-websevice/src/main/java/com/book/springboot/config/auth/SecurityConfig.java
+++ 김수빈/freelec-springboot2-websevice/src/main/java/com/book/springboot/config/auth/SecurityConfig.java
@@ -20,15 +20,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable()
                 .and()
                     .authorizeRequests()
-                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                  //.antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
                     .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                     .anyRequest().authenticated()
                 .and()
-                    .logout().logoutSuccessUrl("/")
+                    .logout()
+                    .logoutSuccessUrl("/")
+
                 .and()
                     .oauth2Login()
-                        .userInfoEndpoint()
-                        .userService(customOAuth2UserService);
+                    .userInfoEndpoint()
+                    .userService(customOAuth2UserService);
 
 
     }
